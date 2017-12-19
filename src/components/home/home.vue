85cf7f047ccc0b891ca659c8138d1f88e68d2ce3
YvesZhang <zhyf0310@163.com>
2017-12-20 00:25:43 +0800

    change the left nav

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,11 +2,11 @@
 <div>
 	<el-row class="tac">
 		<el-col :span="2">
-			<el-menu default-active="1" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" class="leftNav">
-				<el-submenu index="1">
+			<el-menu default-active="1" @open="handleOpen" @close="handleClose"  text-color="#fff" active-text-color="#545c64" class="leftNav">
+				<el-submenu :index="item.index" v-for="item in navList" :key="item.index">
 					<template slot="title">
 			            <i class="fa fa-file-text" style="margin-right:5px;"></i>
-			        	<span>导航一</span>
+			        	<span>{{item.name}}</span>
 			        </template>
 				</el-submenu>
 			</el-menu>
@@ -15,7 +15,7 @@
 </div>
 </template>
 
-<script>
+<script> 
 import "./home.scss"
 
 const navList = require('@/nav.json');
