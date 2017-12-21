45c0606e7fe267dc37187c072e08a96708436b42
YvesZhang <zhyf0310@163.com>
2017-12-22 00:27:50 +0800

    just a bit

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,28 +1,32 @@
 <template>
-<div>
-	<el-menu
-	  :default-active="activeIndex2"
-	  class="el-menu-demo"
-	  mode="horizontal"
-	  @select="handleSelect"
-	  background-color="#545c64"
-	  text-color="#fff"
-	  active-text-color="#ffd04b">
-	  <el-menu-item :index="item.index" v-for="item in navList" :key="item.id" v-if="!item.subNav">{{item.name}}</el-menu-item>
-	  <el-submenu :index="item.index" v-else>
-	    <template slot="title">{{item.name}}</template>
-	    <el-menu-item :index="subItem.index" v-for="subItem in item.subNav" :key="subItem.id">{{subItem.name}}</el-menu-item>
-	  </el-submenu>
-	</el-menu>
+<el-container>
+	<el-header class="header">
+		<el-menu
+		  :default-active="activeIndex2"
+		  class="el-menu-demo"
+		  mode="horizontal"
+		  @select="handleSelect"
+		  background-color="#545c64"
+		  text-color="#fff"
+		  active-text-color="#ffd04b">
+		  <el-menu-item :index="item.index" v-for="item in navList" :key="item.id" v-if="!item.subNav">{{item.name}}</el-menu-item>
+		  <el-submenu :index="item.index" v-else>
+		    <template slot="title">{{item.name}}</template>
+		    <el-menu-item :index="subItem.index" v-for="subItem in item.subNav" :key="subItem.id">{{subItem.name}}</el-menu-item>
+		  </el-submenu>
+		</el-menu>
+	</el-header>
+	<el-main style="background-color:red;">
+		<h3>{{count}}</h3>
+		<div>
+			<el-button type="primary" size="small" @click="countFuc('add')">+</el-button>
+			<el-button type="danger" size="small" @click="countFuc('reduce')">-</el-button>
+		</div>
+	</el-main>
+	<el-footer style="background-color:blue;">
 
-
-
-	<h3>{{count}}</h3>
-	<div>
-		<el-button type="primary" size="small" @click="countFuc('add')">+</el-button>
-		<el-button type="danger" size="small" @click="countFuc('reduce')">-</el-button>
-	</div>
-</div>
+	</el-footer>	
+</el-container>
 </template>
 
 <script> 
@@ -59,12 +63,6 @@ export default {
 	store,
 	methods:{
 		...mapMutations([]),
-		handleOpen(key,keyPath){
-			console.log(key,keyPath)
-		},
-		handleClose(key,keyPath){
-			console.log(key,keyPath)
-		},
 		handleSelect(key, keyPath) {
 	        console.log(key, keyPath);
 	    },
